510d7e834dbed2712d06851f2c2488e1cf8d43e5
linpastone-41323217 <zhouy035@gmail.com>
2025-06-18 21:29:46 +0800

    reeborg拔羅波

--- main.py
+++ main.py
@@ -2,5 +2,6 @@ from cmsimde import flaskapp
 from gevent.pywsgi import WSGIServer
 
 #flaskapp.app.run(host="0.0.0.0", debug=True)
-http_server = WSGIServer(('0.0.0.0', 8080), flaskapp.app)
+#http_server = WSGIServer(('0.0.0.0', 8080), flaskapp.app)
+http_server = WSGIServer(('::', 8080), flaskapp.app, family=socket.AF_INET6)
 http_server.serve_forever()
